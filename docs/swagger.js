ee552ad4985a0f1d5f13a72a7315b5e811a9c431
eddiedhin <sekirangi1@gmail.com>
2025-10-15 00:05:52 +0300

    more twitches

--- docs/swagger.js
+++ docs/swagger.js
@@ -10,7 +10,7 @@ const swaggerDocument = {
   },
   servers: [
     {
-      url: "http://localhost:3000",
+      url: "http://localhost:4800",
       description: "Development server (local)",
     },
     {
